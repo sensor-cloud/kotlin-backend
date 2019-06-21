8b05c31c1f33b19e4ba136203e393ce07555cc93
Em Grasmeder <egrasmed@thoughtworks.com>
2019-06-21 16:43:30 +0200

    initial commit with ktor defaults

---
+++ src/Application.kt
@@ -0,0 +1,73 @@
+package org.sensor-cloud
+
+import io.ktor.application.*
+import io.ktor.response.*
+import io.ktor.features.*
+import io.ktor.routing.*
+import io.ktor.http.*
+import io.ktor.auth.*
+import io.ktor.client.*
+import io.ktor.client.engine.jetty.*
+import io.ktor.client.features.auth.Auth
+import io.ktor.client.features.auth.providers.basic
+import io.ktor.client.features.json.*
+import kotlinx.coroutines.*
+import io.ktor.client.features.logging.*
+
+fun main(args: Array<String>): Unit = io.ktor.server.jetty.EngineMain.main(args)
+
+@Suppress("unused") // Referenced in application.conf
+@kotlin.jvm.JvmOverloads
+fun Application.module(testing: Boolean = false) {
+    install(DefaultHeaders) {
+        header("X-Engine", "Ktor") // will send this header with each response
+    }
+
+    install(Authentication) {
+        basic("myBasicAuth") {
+            realm = "Ktor Server"
+            validate { if (it.name == "test" && it.password == "password") UserIdPrincipal(it.name) else null }
+        }
+    }
+
+    val client = HttpClient(Jetty) {
+        install(Auth) {
+            basic {
+                username = "test"
+                password = "pass"
+            }
+        }
+        install(JsonFeature) {
+            serializer = GsonSerializer()
+        }
+        install(Logging) {
+            level = LogLevel.HEADERS
+        }
+    }
+    runBlocking {
+        // Sample for making a HTTP Client request
+        /*
+        val message = client.post<JsonSampleClass> {
+            url("http://127.0.0.1:8080/path/to/endpoint")
+            contentType(ContentType.Application.Json)
+            body = JsonSampleClass(hello = "world")
+        }
+        */
+    }
+
+    routing {
+        get("/") {
+            call.respondText("HELLO WORLD!", contentType = ContentType.Text.Plain)
+        }
+
+        authenticate("myBasicAuth") {
+            get("/protected/route/basic") {
+                val principal = call.principal<UserIdPrincipal>()!!
+                call.respondText("Hello ${principal.name}")
+            }
+        }
+    }
+}
+
+data class JsonSampleClass(val hello: String)
+
